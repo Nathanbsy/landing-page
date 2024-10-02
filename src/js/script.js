20ab6634278baa5e2f99260c3e8fc17c207217b0
Nathanbsy <nathanbs1227@gmail.com>
2024-10-02 10:12:18 -0300

    Estilizando inicio, serviços e sobre

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,20 @@ function mostraMenu() {
     btn.classList.toggle("ativar");
     menu.classList.toggle("visivel");
 }
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      const navHeight = document.querySelector("nav").offsetHeight; // Pega a altura do nav fixo
+      const offsetTop = targetElement.offsetTop - navHeight;
+  
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth' // Rolagem suave
+      });
+    });
+  });
